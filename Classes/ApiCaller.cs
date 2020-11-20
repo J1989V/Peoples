dc163ba0302dc3589b2d5f05a455d7bc4fb51c2d
Rudi van Tonder <rudi.vantonder@re-leased.com>
2020-11-20 14:01:55 +1300

    UploadController and Login ove api attempt

---
+++ Classes/ApiCaller.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.Owin;
+using Newtonsoft.Json;
+
+namespace Peoples.Classes
+{
+	public class ApiCaller
+	{
+		private string Baseurl;
+
+		public ApiCaller( )
+		{
+			Baseurl = ConfigurationManager.AppSettings.Get( "dbApiUrl" );
+		}
+
+		// public async Task<SignInStatus> LoginCall( ApiLogin apiLogin )
+		public async Task<SignInStatus> LoginCall( ApiLogin apiLogin )
+		{
+			SignInStatus signInStatus = new SignInStatus( );
+
+			using ( var client = new HttpClient( ) )
+			{
+				//Passing service base url  
+				client.BaseAddress = new Uri( Baseurl );
+
+				client.DefaultRequestHeaders.Clear( );
+				//Define request data format  
+				client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue( "application/json" ) );
+
+				//Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+				string json = JsonConvert.SerializeObject( apiLogin );
+				var requestData = new StringContent( json, Encoding.UTF8, "application/json" );
+
+				var responseTask = await client.PostAsync( "api/account/login", requestData );
+
+				//Checking the response is successful or not which is sent using HttpClient  
+				if ( responseTask.IsSuccessStatusCode )
+				{
+					//Storing the response details received from web api   
+					var readTask = responseTask.Content.ReadAsStringAsync( ).Result;
+
+					//Deserializing the response received from web api and storing into the Employee list  
+					signInStatus = JsonConvert.DeserializeObject<SignInStatus>( readTask );
+
+					return signInStatus;
+				}
+			}
+
+			return signInStatus;
+		}
+	}
+
+	public class ApiLogin
+	{
+		public string Email { get; set; }
+
+		public string Password { get; set; }
+	}
+
+	public class ApiLoginResult
+	{
+		public string Result { get; set; }
+
+		public string Token { get; set; }
+	}
+}
---
+++ Controllers/UploadController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Peoples.Controllers
+{
+	public class UploadController : Controller
+	{
+		// GET: Upload  
+		public ActionResult Index( )
+		{
+			return UploadFile( );
+		}
+
+		[HttpGet]
+		public ActionResult UploadFile( )
+		{
+			return View( );
+		}
+
+		[HttpPost]
+		public ActionResult UploadFile( HttpPostedFileBase file )
+		{
+			try
+			{
+				List<string> retStrings = new List<string>( );
+
+				if ( file.ContentLength > 0 )
+				{
+					string _FileName = Path.GetFileName( file.FileName );
+					string fileExtention = Path.GetExtension( _FileName );
+
+					if ( fileExtention.Equals( ".txt" ) )
+					{
+						using ( StreamReader sr = new StreamReader( file.InputStream ) )
+						{
+							string line;
+							while ( ( line = sr.ReadLine( ) ) != null )
+							{
+								retStrings.Add( line );
+							}
+
+							sr.Close( );
+						}
+					}
+
+					//file.SaveAs( _path );
+				}
+
+				ViewBag.Message = "File Uploaded Successfully!!";
+				return View( );
+			}
+			catch
+			{
+				ViewBag.Message = "File upload failed!!";
+				return View( );
+			}
+		}
+	}
+}
